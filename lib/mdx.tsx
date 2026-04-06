79940cc9fc23c4958801b0b8c76fd472cb9af260
Reggie Alleyne <reggiealleyne89@gmail.com>
2026-04-06 13:35:32 -0700

    Upgrade next-mdx-remote for Vercel deploy

--- lib/mdx.tsx
+++ lib/mdx.tsx
@@ -7,6 +7,7 @@ export async function renderArticleMdx(source: string) {
     source,
     components: mdxComponents,
     options: {
+      blockJS: false,
       parseFrontmatter: false,
       mdxOptions: {
         remarkPlugins: [remarkGfm],
